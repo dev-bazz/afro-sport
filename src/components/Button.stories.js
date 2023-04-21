51449cbfac170bcf26bc7186e2d2f93b48f51f4c
bazz <clement.f.bazz@gmail.com>
2023-04-21 20:00:12 +0100

    starter files

---
+++ src/components/Button.stories.js
@@ -0,0 +1,8 @@
+import Button from "./Button.vue";
+
+export default {
+	title: "Buttons/ test",
+	component: Button,
+};
+
+export const Primary = {};
---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<script setup>
+	const label = "Test Button";
+</script>
+
+<template>
+	<button
+		@click="onClick"
+		:disabled="disabled">
+		{{ label }}
+	</button>
+</template>
