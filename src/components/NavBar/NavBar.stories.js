37e035f4986864234870f96dcb18627baf9fc5e0
bazz <clement.f.bazz@gmail.com>
2023-04-22 18:07:11 +0100

    basic setup and logo import

---
+++ src/components/NavBar/NavBar.stories.js
@@ -0,0 +1,8 @@
+import NavBar from "./NavBar.vue";
+
+export default {
+	title: "NavBar",
+	component: NavBar,
+};
+
+export const notLoggedIn = {};
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+	<nav class="nav">
+		<div class="nav__wrapper">
+			<ul>
+				<li>Login</li>
+				<li>Create Account</li>
+			</ul>
+		</div>
+	</nav>
+</template>
+
+<style lang="scss" scoped></style>
