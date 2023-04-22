1ab97f8c102a015a3c7fc16ec87bf72899e80124
bazz <clement.f.bazz@gmail.com>
2023-04-22 19:57:55 +0100

    Navbar

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -3,12 +3,41 @@
 <template>
 	<nav class="nav">
 		<div class="nav__wrapper">
-			<ul>
-				<li>Login</li>
-				<li>Create Account</li>
+			<img
+				src="./assets/afrosport-logo.png"
+				alt="logo" />
+			<ul class="nav__list">
+				<li class="btn__white">Login</li>
+				<li class="btn__black">Create Account</li>
 			</ul>
 		</div>
 	</nav>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	@use "../../styles/utilities.scss" as *;
+	.nav {
+		&__wrapper {
+			@include flexSpaceBetween;
+			max-width: calc(map-get($containers, "max-width") - 16px);
+			margin-inline: auto;
+			padding-inline: pix-rems(16);
+		}
+
+		&__list {
+			@include flex;
+			list-style: none;
+			gap: px-to-rem(24px);
+
+			li {
+				@include fontFamily;
+			}
+			.btn__black {
+				@include button;
+			}
+			.btn__white {
+				@include button(true);
+			}
+		}
+	}
+</style>
