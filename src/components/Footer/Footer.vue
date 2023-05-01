4c77da363d7db2a73168315a2bfc632b4144fd09
bazz <clement.f.bazz@gmail.com>
2023-05-01 09:04:18 +0100

    updated

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,6 +1,31 @@
 <template>
-	<h2>This is the footer</h2>
+	<footer>
+		<div class="container">
+			<ul>
+				<li>Home</li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+	</footer>
 </template>
 
 <script setup></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	@use "../../styles/utilities.scss" as *;
+	footer {
+		background-color: map-get($colors, $key: "black");
+		padding: px-to-rem(24px) px-to-rem(16px);
+
+		.container {
+			max-width: map-get($containers, "max-width");
+			margin-inline: auto;
+
+			ul {
+				color: white;
+			}
+		}
+	}
+</style>
